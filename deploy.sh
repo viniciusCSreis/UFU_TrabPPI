8e022a0771ca16a43de8c718f267e2f916792d8c
viniciuscsreis <viniciuscsreis@gmail.com>
2018-11-17 00:19:50 -0200

    deploy agora so manda a pasta public

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 senha=ufu@123ufu@345
 echo $senha
-ncftpput -R -v -u "vinicius@ejtechmob.com.br" ejtechmob.com.br /public_html/mylifemyhouse/ ./*
+ncftpput -R -v -u "vinicius@ejtechmob.com.br" ejtechmob.com.br /public_html/mylifemyhouse/ ./public/*
 
 #password
 #ufu@123ufu@345
